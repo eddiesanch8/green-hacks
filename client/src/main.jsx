b2bba0760cc072f0e9513c4add54b1229462a0b5
hlian03 <helenalian03@gmail.com>
2025-10-28 18:38:29 -0400

    fixed merge

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,21 +5,19 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import "materialize-css/dist/css/materialize.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import new pages
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import { Hello } from "./app"; // keep your demo component if you want
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-        <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </BrowserRouter>
