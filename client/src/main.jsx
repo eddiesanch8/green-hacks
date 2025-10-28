cbfd35e4ef538ad2f73f381941a9117fa9d2e371
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-10-28 17:59:21 -0400

    completed layouts for header & footer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import "materialize-css/dist/css/materialize.min.css";
-import { Hello } from "./app";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Hello />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </StrictMode>
 );
