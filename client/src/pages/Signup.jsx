fad2630219c9b0cb417645576ba07765d99d2a34
eddiesanch8 <146777616+eddiesanch8@users.noreply.github.com>
2025-10-31 18:36:09 -0400

    Update signup API endpoint to production URL

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -52,7 +52,7 @@ export default function Signup() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/auth/signup", {
+      const res = await fetch("https://green-hacks-production.up.railway.app", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
